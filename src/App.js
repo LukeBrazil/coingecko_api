39e9270de82fc7f30f11b399656930dc5d2095ec
LukeBrazil <Luke.Brazil13@icloud.com>
2021-03-06 16:46:47 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  
+  const [btc, setBtc] = useState([])
+
+  useEffect(() => {
+    axios
+    .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then((response) => {
+      setBtc(response.data[0])
+    }) 
+  }, [])
+  
+  function checkBtc() {
+    console.log('STATE:', btc)
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Hello</h1>
+    <button onClick={checkBtc}>Click Me</button>
+    </>
+    
   );
-}
+  }
 
 export default App;
